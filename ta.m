5527b7459a13ecfcb080790e5102a2c853529866
Raghav1813 <99203611+Raghav1813@users.noreply.github.com>
2022-04-20 22:37:24 +0530

    Delete ta.m

--- ta.m
+++
@@ -1,9 +0,0 @@
-function result = doWhat(dirPath, isCaseSensitive, ~, ~)
-    if isCaseSensitive
-        result = what('-casesensitive', dirPath);
-    else
-        result = what('-caseinsensitive', dirPath);
-    end
-end
-
-%   Copyright 2007-2021 The MathWorks, Inc.
